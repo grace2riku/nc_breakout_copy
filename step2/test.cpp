21aa4b104ea087706052d8af1406bed67c1ae740
Koji Abe <gracehime@gmail.com>
2022-06-10 07:12:32 +0900

    【Step 2: カーソルとキー入力非表示】を写経し、動作確認した。

---
+++ step2/test.cpp
@@ -0,0 +1,13 @@
+#include <ncurses.h>
+
+int main(void) {
+    initscr();
+    noecho();   // キー入力されても表示しない
+    curs_set(0);    // カーソルを非表示
+    mvprintw(12, 30, "Hello World!");
+    while (true) {
+        int ch = getch();
+        if (ch == 'q') break;
+    }
+    endwin();
+}
